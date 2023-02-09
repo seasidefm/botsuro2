cf8f09395f97c66aac930376a1084cd07e37674d
Duke_Ferdinand <doug@dougflynn.dev>
2023-02-09 10:33:09 -0800

    actually call the command this time

--- src/main.ts
+++ src/main.ts
@@ -86,6 +86,14 @@ async function main() {
 					logger.log(
 						`${channel} - ?discord called by ${tags["display-name"]}`
 					);
+
+					await commands.discordCommand(client, {
+						channel,
+						tags,
+						message,
+						self,
+					});
+
 					break;
 
 				case commands.Commands.SubThanks:
