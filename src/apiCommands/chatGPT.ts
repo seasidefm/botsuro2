0af6316934fa68fb7500f3ca799d360cff65c83d
Duke_Ferdinand <doug@dougflynn.dev>
2023-03-02 17:13:03 -0800

    add rate limit and character limit to ai completion

--- src/apiCommands/chatGPT.ts
+++ src/apiCommands/chatGPT.ts
@@ -1,24 +1,38 @@
 import { Configuration, OpenAIApi } from "openai";
 
-export async function heyBotsuro(query: string, maxTokens: number = 500) {
-	const configuration = new Configuration({
-		apiKey: process.env.OPENAI_API_KEY,
-	});
+let aiLock = false;
+export async function heyBotsuro(query: string, maxTokens: number = 250) {
+	try {
+		if (aiLock) {
+			return;
+		}
 
-	const openai = new OpenAIApi(configuration);
+		aiLock = true;
 
-	const model = "gpt-3.5-turbo";
-	const response = await openai.createChatCompletion({
-		model: model,
-		messages: [
-			{
-				role: "user",
-				content: query,
-			},
-		],
-		temperature: 0.2,
-		max_tokens: maxTokens,
-	});
+		const configuration = new Configuration({
+			apiKey: process.env.OPENAI_API_KEY,
+		});
 
-	return response.data.choices[0].message?.content;
+		const openai = new OpenAIApi(configuration);
+
+		const model = "gpt-3.5-turbo";
+		const response = await openai.createChatCompletion({
+			model: model,
+			messages: [
+				{
+					role: "user",
+					content: `${query} in less than ${maxTokens} characters`,
+				},
+			],
+			temperature: 0.2,
+			max_tokens: maxTokens,
+		});
+
+		aiLock = false;
+
+		return response.data.choices[0].message?.content;
+	} catch (error) {
+		aiLock = false;
+		console.error(error);
+	}
 }
